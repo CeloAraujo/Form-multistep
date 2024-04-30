01663a04b157b18577c4dbf91090b8601d0f0df3
celoaraujo <celoparaujo99@gmail.com>
2024-04-30 17:16:46 -0300

    step css/content

--- multistep-form-react/src/App.jsx
+++ multistep-form-react/src/App.jsx
@@ -6,6 +6,7 @@ import { FiSend } from "react-icons/fi";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
 // hooks
 import { useForm } from "./hooks/useForm";
@@ -27,7 +28,7 @@ function App() {
           </p>
         </div>
         <div className="form-container">
-          <p>etapas</p>
+         <Steps currentStep = {currentStep}/>
           <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
             <div className="inputs-container">{currentComponent}</div>
             <div className="action">
