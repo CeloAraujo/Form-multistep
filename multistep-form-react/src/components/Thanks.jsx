e34794f269ed040e4a0929447ee3814268adbf5c
celoaraujo <celoparaujo99@gmail.com>
2024-04-30 17:49:31 -0300

    Thanks construction e Css

--- multistep-form-react/src/components/Thanks.jsx
+++ multistep-form-react/src/components/Thanks.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import "./Thanks.css";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
 
 const Thanks = () => {
   return (
-    <div>
-        <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>
+        Sua opinião é muito importante,avalie e ganhe benefícios na próxima
+        compra.
+      </p>
+      <h3>Resumo da avaliação:</h3>
+      <p className="review-data">
+        <span>Satisfação:</span>
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Thanks
+export default Thanks;
